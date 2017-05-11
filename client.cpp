69931d6982893d3cda165c67a29199bbfe5aaf3d
Steven XU <hanstxu@gmail.com>
2017-05-11 16:46:46 -0700

    client.cpp: added socket systems calls so that client can send data over a udp connection

--- client.cpp
+++ client.cpp
@@ -1,7 +1,17 @@
 #include <string>
 #include <thread>
 #include <iostream>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#include <string.h>		// for memset
+#include <unistd.h>		// POSIX operating system API
 using namespace std;
+
+// maybe change
+#define BUFFER_SIZE 512
 
 int main(int argc, char* argv[]) {
 	if (argc != 4) {
@@ -28,5 +38,37 @@ int main(int argc, char* argv[]) {
 	cout << "Port Number: " << port_num << endl;
 	cout << "File name: " << argv[3] << endl;
 	
+	int status;
+	struct addrinfo hints;
+	struct addrinfo *servinfo;		// will point to the results
+	
+	memset(&hints, 0, sizeof hints);	// make sure the struct is empty
+	hints.ai_family = AF_INET;			// support only IPv4 (36th post in piazza)
+	hints.ai_socktype = SOCK_DGRAM;	// UDP stream sockets
+	
+	// get ready to connect
+	status = getaddrinfo(argv[1], argv[2], &hints, &servinfo);
+	if (status != 0) {
+		cerr << "ERROR: hostname or ip address is invalid\n";
+		exit(EXIT_FAILURE);
+	}
+	
+	// make a socket
+	int sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+	if (sockfd < 0) {
+		cerr << "ERROR: unable to make a socket" << endl;
+		freeaddrinfo(servinfo);
+		exit(EXIT_FAILURE);
+	}
+	
+	// connect
+	status = connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen);
+	
+	int num_bytes = 11;
+	
+	send(sockfd, "hello world", num_bytes, 0);
+	
+	close(sockfd);
+	freeaddrinfo(servinfo);
 	return 0;
 }
