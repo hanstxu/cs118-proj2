357f98c5f31d970064d4839a612636c34105e3eb
Steven XU <hanstxu@gmail.com>
2017-05-11 16:00:26 -0700

    client.cpp: added argument handling

--- client.cpp
+++ client.cpp
@@ -1,8 +1,32 @@
 #include <string>
 #include <thread>
 #include <iostream>
+using namespace std;
 
-int main()
-{
-  std::cerr << "client is not implemented yet" << std::endl;
+int main(int argc, char* argv[]) {
+	if (argc != 4) {
+		cerr << "ERROR: You must give three arguments when initializing the server.\n";
+		exit(EXIT_FAILURE);
+	}
+	
+	int port_num = 0;
+	
+	try {
+		port_num = stoi(argv[2]);
+	}
+	catch(const invalid_argument& ia) {
+		cerr << "ERROR: invalid argument - " << ia.what() << '\n';
+		exit(EXIT_FAILURE);
+	}
+	
+	if (port_num < 1023 || port_num > 65535) {
+		cerr << "ERROR: You must choose a port number between 1024 and 65535.\n";
+		exit(EXIT_FAILURE);
+	}
+	
+	cout << "IP Address: " << argv[1] << endl;
+	cout << "Port Number: " << port_num << endl;
+	cout << "File name: " << argv[3] << endl;
+	
+	return 0;
 }
