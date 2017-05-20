03767b8f34df284708d9a6bd02abce2c51b09195
Steven XU <hanstxu@gmail.com>
2017-05-19 21:01:45 -0700

    packet.h, packet.cpp, header.h, header.cpp: prepared function headers for hton and ntoh usage

--- packet.cpp
+++ packet.cpp
@@ -2,8 +2,8 @@
 
 //Send packet declaration. Take in syn, ack, cid, flags, payload size and set the m_header accordingly.
 
-Packet::Packet(unsigned int syn, unsigned int ack, unsigned short cid,
-	unsigned short flags, unsigned int payload_size) {
+Packet::Packet(uint32_t syn, uint32_t ack, uint16_t cid, uint16_t flags,
+	unsigned int payload_size) {
 	m_syn = 0, m_ack = 0, m_cid = 0, m_flags = 0;
 	convert_to_buffer(m_header, syn, ack, cid, flags);
 	m_payload_size = payload_size;
@@ -14,11 +14,7 @@ Packet::Packet(unsigned int syn, unsigned int ack, unsigned short cid,
 //Receive packet declaration. Take in buffer and set the m_syn, m_ack, m_cid, m_flags accordingly.
 Packet::Packet(unsigned char* buffer, unsigned int payload_size) {
 	memset(m_header, 0, HEADER_SIZE);
-	// for (int i = 0; i < 12; i++)
-	// 	printf("%x ", (unsigned char)buffer[i]);
-	// cout << endl;
 	get_header_info(buffer, m_syn, m_ack, m_cid, m_flags);
-	// cout << m_syn << endl;
 	m_payload_size = payload_size;
 	m_payload = new unsigned char[payload_size];
 	memcpy(m_payload, &buffer[12], m_payload_size);
